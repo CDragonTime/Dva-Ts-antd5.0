ff79fa3f3cdcf45426af78812e9d6b7a7f0c2c15
chenlong <chenlong3@webull.com>
2023-02-27 13:53:57 +0800

    feat: 添加样式兼容

--- src/config/router.js
+++ src/config/router.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Router, Route, Switch, Redirect } from 'dva/router';
 // import IndexPage from './routes/Index/IndexPage.tsx';
-import ReactArray from '@src/routes/ReactArray/Index';
-import PositionTest from '@src/routes/PositionTest/Index';
-import ServiceWorker from '@src/routes/ServiceWorker/index';
+import ReactArray from '@src/routes/ReactArray/Index.jsx';
+import PositionTest from '@src/routes/PositionTest/Index.jsx';
+// import ServiceWorker from '@src/routes/ServiceWorker/index';
 
 function RouterConfig({ history }) {
 
@@ -13,7 +13,7 @@ function RouterConfig({ history }) {
         {/* <Route path="/" exact component={IndexPage} /> */}
         <Route path="/reactArray" exact component={ReactArray} />
         <Route path="/position" exact component={PositionTest} />
-        <Route path="/serviceWorker" exact component={PositionTest} />
+        {/* <Route path="/serviceWorker" exact component={PositionTest} /> */}
         <Redirect path="/" to="/position"/>
       </Switch>
     </Router>
